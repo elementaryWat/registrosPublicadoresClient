7aa17f0fad1d4f7be6e058a9ce88a5d455d6175b
Augusto Romero <geragusto@hotmail.com>
2018-05-07 22:48:27 -0300

    FontAwesome con CSS

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -15,16 +15,17 @@ export class NavbarComponent implements OnInit {
   
   ngAfterViewInit() {
     // Force the toggled class to be removed when a collapsible nav link is clicked
-    $(".navbar-sidenav .nav-link-collapse").click((e) => {
-      e.preventDefault();
-      $("body").removeClass("sidenav-toggled");
-    });
-    $('#sidenavToggler').click((e)=>{
+    $("#sidenavToggler").click(function(e) {
       e.preventDefault();
       $("body").toggleClass("sidenav-toggled");
       $(".navbar-sidenav .nav-link-collapse").addClass("collapsed");
       $(".navbar-sidenav .sidenav-second-level, .navbar-sidenav .sidenav-third-level").removeClass("show");
-    })
+    });
+    // Force the toggled class to be removed when a collapsible nav link is clicked
+    $(".navbar-sidenav .nav-link-collapse").click(function(e) {
+      e.preventDefault();
+      $("body").removeClass("sidenav-toggled");
+    });
   }
 
 }
