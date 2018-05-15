1a26770953aaab125020491c67294b0a17355952
Augusto Romero <geragusto@hotmail.com>
2018-05-14 22:56:09 -0300

    Eliminar Publicador Modal

---
+++ src/app/components/publicadores/eliminar/eliminar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eliminar-publicador',
+  templateUrl: './eliminar-publicador.component.html',
+  styles: []
+})
+export class EliminarPublicadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
